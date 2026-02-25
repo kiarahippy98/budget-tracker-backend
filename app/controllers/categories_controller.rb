1cbf3f8317fe6059c6995cbe1962ee03a5bfd441
Micaela February <micaela.february9@gmail.com>
2026-02-25 13:10:11 +0200

    fixed ApplicationController, enable sessions, fixed Categories controller, Devise login working

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,51 @@
+# app/controllers/categories_controller.rb
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!          # Devise auth
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = current_user.categories
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to @category, notice: "Category created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: "Category updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "Category deleted."
+  end
+
+  private
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
